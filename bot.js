f59e06bbea3c207691a0675beca84b6a57649903
iiYazan <45070056+iiYazan@users.noreply.github.com>
2018-12-22 05:52:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+console.log("Scrpit By YAZAN");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("513463322830700548")
+let channel =     client.channels.get("526033450302701589")
 setInterval(function() {
 channel.send(`yazan``pro`);
 }, 30)
